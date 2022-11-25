9f9259235dcbbf85c85a3eabecd57e36713fd67e
SaadiaElf <saadiaelfekak@gmail.com>
2022-11-25 00:47:27 +0000

    Present the High Score Tables sorted alphabetically by country name

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from "react";
 import Card from "./Components/Card";
 import allCountryScores from "./scores";
 import "./App.css";
-const sortedValues = allCountryScores.sort();
-console.log(sortedValues);
+
+const sortedCountries = allCountryScores.sort((a, b) => {
+  let countryA = a.name.toLowerCase();
+  let countryB = b.name.toLowerCase();
+
+  if (countryA < countryB) {
+    return -1;
+  }
+  if (countryA > countryB) {
+    return 1;
+  }
+  return 0;
+});
+
 function App() {
   return (
     <div className="container">
       <h1 className="heading"> High Score by Country</h1>
-      {sortedValues.map((country, index) => (
+      {sortedCountries.map((country, index) => (
         <Card key={index} country={country.name} scores={country.scores} />
       ))}
     </div>
