275b2e882d15a938641556f3bdc0a5967e453dcf
SaadiaElf <saadiaelfekak@gmail.com>
2022-12-01 17:57:32 +0000

    Sorting scores

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,10 +2,14 @@ import React from "react";
 import Score from "./Score";
 
 const Card = (props, key) => {
+  const sortedScores = props.scores.sort((a, b) => {
+    return b.s - a.s;
+  });
+
   return (
     <div className="card" key={key}>
       <h2 className="card__title">HighScores : {props.country}</h2>
-      {props.scores.map((item, index) => {
+      {sortedScores.map((item, index) => {
         return <Score key={index} name={item.n} score={item.s} />;
       })}
     </div>
