1ea64fa3a5e4f30fa03db4fbf589c612b7008ac5
SaadiaELF <saadiaelfekak@gmail.com>
2022-12-09 16:53:46 +0000

    Refactoring

--- src/App.js
+++ src/App.js
@@ -2,31 +2,23 @@ import React, { useState } from "react";
 import Card from "./Components/Card";
 import allCountryScores from "./scores";
 import "./App.css";
+
 function App() {
   const sortedCountries = allCountryScores.sort((a, b) => {
     let countryA = a.name.toLowerCase();
     let countryB = b.name.toLowerCase();
     return countryA < countryB ? -1 : countryA > countryB ? 1 : 0;
   });
-
   const [countries, setCountries] = useState(sortedCountries);
   const [order, setOrder] = useState(true);
 
   function orderScores() {
     if (order) {
-      countries.map((country) =>
-        country.scores.sort((a, b) => {
-          return b.s - a.s;
-        })
-      );
+      countries.map((country) => country.scores.sort((a, b) => b.s - a.s));
       setCountries(countries);
       setOrder(false);
     } else {
-      countries.map((country) =>
-        country.scores.sort((a, b) => {
-          return a.s - b.s;
-        })
-      );
+      countries.map((country) => country.scores.sort((a, b) => a.s - b.s));
       setCountries(countries);
       setOrder(true);
     }
